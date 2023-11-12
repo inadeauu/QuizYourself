29bf8e0ec0eda80d52e4af0ba6acb4009e19d767
Pr0gramm3r404 <123025115+Pr0gramm3r404@users.noreply.github.com>
2023-11-11 22:33:33 -0500

    dropdown close

--- client/src/components/nav/ProfileDropdown.tsx
+++ client/src/components/nav/ProfileDropdown.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { AiOutlineProfile } from "react-icons/ai"
 import { HiXMark } from "react-icons/hi2"
 import { useAuth } from "../../hooks/useAuth"
 import { CgProfile } from "react-icons/cg"
 import { trpc } from "../../utils/trpc"
 import { Link, useNavigate } from "react-router-dom"
+import useClickOutside from "../../hooks/useClickOutside"
 
 const ProfileDropdown = () => {
   const { user } = useAuth()
   const [open, setOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
 
   const utils = trpc.useUtils()
 
   const navigate = useNavigate()
+
+  useClickOutside(menuRef, () => {
+    if (open) {
+      setOpen(false)
+    }
+  })
 
   if (!user) return null
 
@@ -24,12 +32,16 @@ const ProfileDropdown = () => {
   })
 
   return (
-    <div className="relative">
+    <div ref={menuRef} className="relative">
       <div
         className="hover:cursor-pointer"
         onClick={() => setOpen((prev) => !prev)}
       >
-        {open ? <HiXMark size={32} /> : <AiOutlineProfile size={32} />}
+        {open ? (
+          <HiXMark size={32} className="pointer-events-none" />
+        ) : (
+          <AiOutlineProfile size={32} className="pointer-events-none" />
+        )}
       </div>
       {open && (
         <div className="absolute bg-neutral-50 border-2 rounded-sm p-4 right-0 mt-2 w-[200px]">
